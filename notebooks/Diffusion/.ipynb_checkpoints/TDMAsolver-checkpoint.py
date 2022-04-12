da5cb29ea2ed875dedbc1f4a5daa2a1b828cc49d
jeanbraun <jeanbraun@me.com>
2022-04-12 17:08:08 +0200

    first commit

---
+++ notebooks/Diffusion/.ipynb_checkpoints/TDMAsolver-checkpoint.py
@@ -0,0 +1,30 @@
+import numba
+@numba.njit
+## Tri Diagonal Matrix Algorithm(a.k.a Thomas algorithm) solver
+def TDMAsolver(a, b, c, d):
+    '''
+    TDMA solver, a b c d can be NumPy array type or Python list type.
+    refer to http://en.wikipedia.org/wiki/Tridiagonal_matrix_algorithm
+    and to http://www.cfd-online.com/Wiki/Tridiagonal_matrix_algorithm_-_TDMA_(Thomas_algorithm)
+    a is lower diagonal
+    b is diagonal
+    c is upper diagonal
+    d is rhs
+    '''
+    nf = len(d) # number of equations
+    ac = a.copy()
+    bc = b.copy()
+    cc = c.copy()
+    dc = d.copy()
+    for it in range(1, nf):
+        mc = ac[it-1]/bc[it-1]
+        bc[it] = bc[it] - mc*cc[it-1] 
+        dc[it] = dc[it] - mc*dc[it-1]
+        	    
+    xc = bc
+    xc[-1] = dc[-1]/bc[-1]
+
+    for il in range(nf-2, -1, -1):
+        xc[il] = (dc[il]-cc[il]*xc[il+1])/bc[il]
+
+    return xc
